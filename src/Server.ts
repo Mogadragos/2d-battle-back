9d65498b6cec67496e0e786e46bc16d85c03e14d
Mogador <jcaillaud17170@gmail.com>
2023-08-26 12:55:27 +0200

    Create Server class 2

---
+++ src/Server.ts
@@ -0,0 +1,44 @@
+import { Server as ServerIO } from "socket.io";
+import { Socket } from "./shared-types";
+
+export class Server extends ServerIO<
+    Socket.ClientToServerEvents,
+    Socket.ServerToClientEvents,
+    Socket.InterServerEvents,
+    Socket.SocketData
+> {
+    constructor() {
+        super(3000, {
+            cors: {
+                origin: "http://localhost:5173",
+                methods: ["GET", "POST"],
+            },
+        });
+
+        this.init();
+
+        console.log("Server Started !");
+    }
+
+    init() {
+        this.on("connection", function (socket) {
+            console.log("user connected");
+
+            socket.on("disconnecting", (reason) => {
+                console.log("user disconnecting");
+                global.eventManager.dispatchEvent("disconnecting", socket);
+            });
+
+            if (socket.rooms.size < 2) {
+                global.eventManager.dispatchEvent("findGame", socket);
+            }
+        });
+
+        global.eventManager.addEventListener(
+            "workerStopped",
+            (room: string) => {
+                this.to(room).emit("close");
+            }
+        );
+    }
+}
